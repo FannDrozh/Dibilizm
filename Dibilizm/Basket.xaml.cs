c4030c1ea1030c77364644af98e128bd52cdf6f1
Илья Барашенков <ya.ilya-03@ya.ru>
2023-06-09 00:04:48 +0300

    Добавьте файлы проекта.

---
+++ Dibilizm/Basket.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dibilizm
+{
+    /// <summary>
+    /// Логика взаимодействия для Basket.xaml
+    /// </summary>
+    public partial class Basket : Window
+    {
+        List<Table> tables = new List<Table>();
+
+        public Basket()
+        {
+            InitializeComponent();
+            tables = Entities.GetContext().Table.ToList();
+            LBasket.ItemsSource = tables;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Zakaz_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            printDialog.PrintVisual(LBasket, "");
+            Entities.GetContext().Table.RemoveRange(tables);
+            Entities.GetContext().SaveChanges();
+            LBasket.ItemsSource = tables;
+        }
+        private async void Minus_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            await Task.Delay(100);
+            List<Table> t = new List<Table> { new Table() };
+            List<Table> t1 = Entities.GetContext().Table.ToList();
+            t[0] = LBasket.SelectedItem as Table;
+            for (int i = 0; i < t1.Count; i++)
+            {
+                if (t1[i].id == t[i].id)
+                {
+                    t1[i].Koli -= 1;
+                }
+            }
+            for (int i = 0; i < t1.Count; i++)
+            {
+                if (t1[i].id == t[i].id)
+                {
+                    if (t1[i].Koli == 0)
+                    {
+                        Entities.GetContext().Table.Remove(t[i]);
+                    }
+                }
+            }
+            Entities.GetContext().SaveChanges();
+            LBasket.ItemsSource = Entities.GetContext().Table.ToList();
+            int c = 0;
+            for (int i = 0; c < t1.Count; i++)
+            {
+                c += Convert.ToInt32(t1[i].MinCostForAgent) * Convert.ToInt32(t1[i].Koli);
+            }
+            PriceP.Content = Convert.ToString(c + " руб.");
+        }
+
+        private async void Plus_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            await Task.Delay(100);
+            List<Table> t = new List<Table> { new Table() };
+            List<Table> t1 = Entities.GetContext().Table.ToList();
+            t[0] = LBasket.SelectedItem as Table;
+            for (int i = 0; i < t1.Count; i++)
+            {
+                if (t1[0].id == t[0].id)
+                {
+                    t1[0].Koli += 1;
+                }
+            }
+            Entities.GetContext().SaveChanges();
+            LBasket.ItemsSource = tables;
+            int c = 0;
+            for (int i = 0; i < t1.Count; i++)
+            {
+                c += Convert.ToInt32(t1[i].MinCostForAgent) * Convert.ToInt32(t1[i].Koli);
+            }
+            PriceP.Content = Convert.ToString(c + " руб.");
+        }
+    }
+}
---
+++ Dibilizm/Pages/Glavnaya.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dibilizm.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Glavnaya.xaml
+    /// </summary>
+    public partial class Glavnaya : Page
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Frame frame1;
+        List<Product> products = new List<Product>();
+        List<Table> tables = new List<Table> { new Table() };
+        public Glavnaya(Frame frame)
+        {
+            InitializeComponent();
+            products = Entities.GetContext().Product.ToList();
+            ProdL.ItemsSource = products;
+            frame1 = frame;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            object item = ProdL.SelectedItem;
+            List<Product> p = new List<Product> { new Product() };
+            List<Table> t1 = Entities.GetContext().Table.ToList();
+            p[0] = (Product)item;
+            tables[0].Title = p[0].Title;
+            tables[0].Koli = 1;
+            tables[0].MinCostForAgent = p[0].MinCostForAgent;
+            tables[0].ArticleNumber = p[0].ArticleNumber;
+            if (t1.Count == 0)
+            {
+                Entities.GetContext().Table.Add(tables[0]);
+            }
+            else
+            {
+                for(int i = 0; i < t1.Count; i++)
+                {
+                    if (p[0].Title == t1[i].Title)
+                    {
+                        t1[i].Koli += 1;
+                        break;
+                    }
+                    else
+                    {
+                        Entities.GetContext().Table.Add(tables[0]);
+                        break;
+                    }
+                }
+            }
+            Entities.GetContext().SaveChanges();
+            MessageBox.Show("Продукт добавлен в корзину", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+            Basket backet = new Basket();
+            backet.ShowDialog();
+
+
+
+        }
+
+        private void DElProd_Click(object sender, RoutedEventArgs e)
+        {
+            object item = ProdL.SelectedItem;
+            List<Product> products1 = new List<Product> { new Product() };
+            products1[0] = (Product)item;
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].ID == products1[0].ID)
+                {
+                    Entities.GetContext().Product.Remove(products[i]);
+                    Entities.GetContext().SaveChanges();
+                    frame1.Navigate(new Glavnaya(frame1));
+                }
+            }
+        }
+
+        private void BasketProd_Click(object sender, RoutedEventArgs e)
+        {
+            Basket basket = new Basket();
+            basket.ShowDialog();
+        }
+    }
+}
